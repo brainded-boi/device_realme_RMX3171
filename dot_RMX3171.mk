1ef9e8dc274a7e616cebc85713af5724f21a3e8a
Vishesh Raghuvanshi <67827886+visheshraghuvanshi@users.noreply.github.com>
2021-09-10 01:07:41 -0700

    DotOS bringup

--- dot_RMX3171.mk
+++ dot_RMX3171.mk
@@ -21,17 +21,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX3171/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX3171
+PRODUCT_NAME := dot_RMX3171
 PRODUCT_DEVICE := RMX3171
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme Narzo 30A
 PRODUCT_MANUFACTURER := realme
 
 TARGET_BOOT_ANIMATION_RES := 720
+
+# Official dotOS
+DOT_OFFICIAL := false
 
 # Build info
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
