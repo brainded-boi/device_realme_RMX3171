3b599688bbfc53a4498ffb485ae96248439df8c9
Vishesh Raghuvanshi <67827886+visheshraghuvanshi@users.noreply.github.com>
2021-09-13 23:24:18 -0700

    RMX3171 : Added Rounded Gboard Corners

--- system_prop.mk
+++ system_prop.mk
@@ -104,6 +104,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.wfd.nohdcp=1 \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
+# GBoard
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.ime.kb_pad_port_b=1 \
+    ro.com.google.ime.corner_key_r=35
 
 # Wlan
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
